1ef8f6418ff2340a18bde165c39742280e429370
Shwetank Singh <shwetank.s@happylocate.com>
2021-04-19 18:41:45 +0530

    Change: Update configuration file

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     DEVELOPMENT = False
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
